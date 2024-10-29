7a2b4bfb24f2ffdb43768aa48167256944e570df
unknown <peralta@exapmple.com>
2024-10-29 08:16:14 -0500

    add models

---
+++ app/Models/Autor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+    use HasFactory;
+    protected $table = 'autor';
+    protected $primaryKey = 'idAutor';
+    protected $fillable = [
+        'nombreAU',
+    ];
+}
---
+++ app/Models/Reader.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reader extends Model
+{
+    use HasFactory;
+    protected $table = 'lector';
+    protected $primaryKey = 'idLector';
+    protected $fillable = [
+        'nombre',
+        'APaterno',
+        'AMaterno',
+        'DNI',
+        'codigoLector',
+        'semestre',
+        'idTipo',
+        'idArea'
+
+    ];
+}
