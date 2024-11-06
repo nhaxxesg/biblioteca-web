c95b7d9d10d60dcce633b24fb228f81a6a684089
Rodrigo <you@example.com>
2024-11-06 11:09:54 -0500

    Cambios en el mainmenu

--- resources/js/Components/AsideBar.jsx
+++ resources/js/Components/AsideBar.jsx
@@ -3,7 +3,7 @@ import NavLink from '@/Components/NavLink';
 function Asidebar() {
     return (
         <aside id="sidebar" class="bg-[#232d30] text-white w-64 min-h-screen p-6 hidden md:block">
-            <h2 class="text-2xl font-bold mb-6">Menú</h2>
+            <h2 class="text-2xl font-bold mb-6">Biblioteca</h2>
             <nav class="space-y-4">
 
                     <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
